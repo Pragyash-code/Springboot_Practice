7abd9a0dd143b2628336e6d75565d7fc010b7e35
Pragyash <pragyashec1112@gmail.com>
2023-04-20 18:20:36 +0530

    changes in Application and entity

--- src/main/java/com/application/entities/Student.java
+++ src/main/java/com/application/entities/Student.java
@@ -13,27 +13,36 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Entity
 @Data
-@Table(name="students")
+@Entity
+@Table(name = "student")
 public class Student {
-	
+
 	@Id
-	@Column(name="rollNo")
+	@Column(name = "rollNo", length = 10)
 	private int rollNo;
+
+	@Column(name = "name", length = 50)
+	private String name;
+
+	@Column(name = "className", length = 50)
+	private String className;
+
+	@Column(name = "school", length = 50)
+	private String school;
+
+	@Column(name = "age", length = 50)
+	private int age;
+
+	@Column(name = "address", length = 50)
+	private String address;
+
+	@Column(name = "gender", length = 50)
+	private String gender;
 	
-	@Column(name="firstName")
-	private String firstName;
-	
-	@Column(name="lastName")
-	private String lastName;
-	
-	@Column(name="standard")
-	private String standard;
-	
-	//mapping subject entity class with student class using oneToMany mapping
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name="rollNo", referencedColumnName="rollNo")
-	private List<Subject> subjectData;
+//	mapping subject entity class with student class using oneToMany mapping
+//	@OneToMany(cascade = CascadeType.ALL)
+//	@JoinColumn(name="rollNo", referencedColumnName="rollNo")
+//	private List<Subject> subjectData;
 	
 }
